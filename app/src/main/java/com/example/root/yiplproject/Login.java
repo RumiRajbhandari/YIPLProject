90c313695108ff32143efbe0cce2b57f17546336
anjanaraut <anjanaraut@outlook.com>
2017-08-20 14:40:29 +0545

    FIxed login

--- app/src/main/java/com/example/root/yiplproject/Login.java
+++ app/src/main/java/com/example/root/yiplproject/Login.java
@@ -15,12 +15,12 @@ import com.backendless.exceptions.BackendlessFault;
 import com.example.root.yiplproject.model.Comment;
 
 public class Login extends AppCompatActivity {
-    Button signup,login,forum;
-    EditText email,password;
-    public String mail,pass;
+    Button signup, login, forum;
+    EditText email, password;
+    public String mail, pass;
     Comment comment;
     public String userid;
-    String TAG="TAG";
+    String TAG = "TAG";
     BackendlessUser userLogIn;
 
     @Override
@@ -28,24 +28,21 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        signup=(Button)findViewById(R.id.btn_register);
-        login=(Button)findViewById(R.id.btn_login);
-        email=(EditText)findViewById(R.id.txt_email);
-        password=(EditText)findViewById(R.id.txt_password);
-        forum=(Button)findViewById(R.id.btn_forum);
+        signup = (Button) findViewById(R.id.btn_register);
+        login = (Button) findViewById(R.id.btn_login);
+        email = (EditText) findViewById(R.id.txt_email);
+        password = (EditText) findViewById(R.id.txt_password);
+        forum = (Button) findViewById(R.id.btn_forum);
 
-        comment=new Comment();
+        comment = new Comment();
         comment.setAuthorEmail("rumi@gmail.com");
         comment.setMessage("Hello world");
-
-
 
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-               Backendless.initApp(getApplicationContext(),"F4C26AE7-9CA0-8CB8-FF49-D66D9F1C0D00", "9D8E4C70-E734-CAA5-FFFD-B69BAE068400");
+                Backendless.initApp(getApplicationContext(), "F4C26AE7-9CA0-8CB8-FF49-D66D9F1C0D00", "9D8E4C70-E734-CAA5-FFFD-B69BAE068400");
                 loginUserAsync();
 //                passMessage();
 //                subscribeMessage();
@@ -55,7 +52,7 @@ public class Login extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Login.this,SignUp.class);
+                Intent intent = new Intent(Login.this, SignUp.class);
                 startActivity(intent);
             }
         });
@@ -63,49 +60,45 @@ public class Login extends AppCompatActivity {
         forum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Login.this,ForumActivity.class);
+                Intent intent = new Intent(Login.this, ForumActivity.class);
                 startActivity(intent);
             }
         });
 
     }
-    private  void loginUserAsync()
-    {
-        mail=email.getText().toString();
-        pass=password.getText().toString();
-        Log.e("TAG", "loginUser: "+mail+pass );
-        AsyncCallback<BackendlessUser> callback = new AsyncCallback<BackendlessUser>()
-        {
+
+    private void loginUserAsync() {
+        mail = email.getText().toString();
+        pass = password.getText().toString();
+        Log.e("TAG", "loginUser: " + mail + pass);
+        AsyncCallback<BackendlessUser> callback = new AsyncCallback<BackendlessUser>() {
             @Override
-            public void handleResponse( BackendlessUser loggedInUser )
-            {
-                userLogIn=loggedInUser;
-                System.out.println( "User has been logged in - " + loggedInUser.getObjectId() );
-                userid=loggedInUser.getObjectId();
-                Log.e(TAG, "handleResponse: user is :  "+loggedInUser.toString() );
-                Log.e(TAG, "userid1is: "+userid);
-                Log.e(TAG, "handleResponse: "+loggedInUser.getUserId() );
-                if (userid!=null){
-                    Intent intent=new Intent(Login.this, ComplainActivity.class);
-                    intent.putExtra("rumi",userid);
-                    Log.e(TAG, "userid:2 "+userid );
+            public void handleResponse(BackendlessUser loggedInUser) {
+                userLogIn = loggedInUser;
+                System.out.println("User has been logged in - " + loggedInUser.getObjectId());
+                userid = loggedInUser.getObjectId();
+                Log.e(TAG, "handleResponse: user is :  " + loggedInUser.toString());
+                Log.e(TAG, "userid1is: " + userid);
+                Log.e(TAG, "handleResponse: " + loggedInUser.getUserId());
+                if (userid != null) {
+                    Intent intent = new Intent(Login.this, ComplainActivity.class);
+                    intent.putExtra("rumi", userid);
+                    Log.e(TAG, "userid:2 " + userid);
                     startActivity(intent);
+                    finish();
                 }
             }
 
             @Override
-            public void handleFault( BackendlessFault backendlessFault )
-            {
-                System.out.println( "Server reported an error - " + backendlessFault.getMessage() );
+            public void handleFault(BackendlessFault backendlessFault) {
+                System.out.println("Server reported an error - " + backendlessFault.getMessage());
             }
         };
 
-        Backendless.UserService.login( mail, pass , callback );
-
+        Backendless.UserService.login(mail, pass, callback, true);
 
 
     }
-
 
 
 }
--- app/src/main/java/com/example/root/yiplproject/MainActivity.java
+++ app/src/main/java/com/example/root/yiplproject/MainActivity.java
@@ -7,27 +7,31 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.backendless.Backendless;
+import com.backendless.async.callback.BackendlessCallback;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView contrator,complain,forum,aboutus;
+    ImageView contrator, complain, forum, aboutus;
     TextView glossary;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        contrator=(ImageView) findViewById(R.id.contractor);
-        complain=(ImageView)findViewById(R.id.complain);
-        forum=(ImageView)findViewById(R.id.forum);
-        aboutus=(ImageView)findViewById(R.id.aboutus);
-        glossary=(TextView)findViewById(R.id.glossary);
+        contrator = (ImageView) findViewById(R.id.contractor);
+        complain = (ImageView) findViewById(R.id.complain);
+        forum = (ImageView) findViewById(R.id.forum);
+        aboutus = (ImageView) findViewById(R.id.aboutus);
+        glossary = (TextView) findViewById(R.id.glossary);
 
         contrator.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ListofContractor.class);
-                intent.putExtra("rumi","rumi");
+                intent.putExtra("rumi", "rumi");
                 startActivity(intent);
             }
         });
@@ -35,16 +39,29 @@ public class MainActivity extends AppCompatActivity {
         complain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,Login.class);
-                startActivity(intent);
+                Backendless.UserService.isValidLogin(new BackendlessCallback<Boolean>() {
+                    @Override
+                    public void handleResponse(Boolean response) {
+                        String loggedInUser = Backendless.UserService.loggedInUser();
+                        Intent intent;
+                        if (response){
 
+                            intent = new Intent(MainActivity.this, ComplainActivity.class);
+                            intent.putExtra("rumi", loggedInUser);
+                        }
+                        else
+                            intent = new Intent(MainActivity.this, Login.class);
+
+                        startActivity(intent);
+                    }
+                });
             }
         });
 
         forum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,ForumActivity.class);
+                Intent intent = new Intent(MainActivity.this, ForumActivity.class);
                 startActivity(intent);
 
 
@@ -54,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         aboutus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,AboutUs.class);
+                Intent intent = new Intent(MainActivity.this, AboutUs.class);
                 startActivity(intent);
 
             }
@@ -62,11 +79,13 @@ public class MainActivity extends AppCompatActivity {
         glossary.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,Glossary.class);
+                Intent intent = new Intent(MainActivity.this, Glossary.class);
                 startActivity(intent);
 
             }
         });
+
+        Backendless.initApp(getApplicationContext(), "F4C26AE7-9CA0-8CB8-FF49-D66D9F1C0D00", "9D8E4C70-E734-CAA5-FFFD-B69BAE068400");
     }
 }
 
