ea664aff110b266da1a96abfa206ac6f90afd8cf
anjanaraut <anjanaraut@outlook.com>
2017-08-22 15:30:31 +0545

    fixed about us

--- app/src/main/java/com/example/root/yiplproject/AboutUs.java
+++ app/src/main/java/com/example/root/yiplproject/AboutUs.java
@@ -1,7 +1,9 @@
 package com.example.root.yiplproject;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class AboutUs extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class AboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        Intent intent=getIntent();
+        String value = intent.getStringExtra("Anjana");
     }
 }
